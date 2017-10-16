e7c94e28572d9faf44358cd935b8873eb41784dc
nicholaskreissler <32829030+nicholaskreissler@users.noreply.github.com>
2017-10-16 00:37:27 -0500

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -112,4 +112,4 @@ while(True):  # Final stages of the program, puts the elevator in a loop allowin
         end = eval(end)
     #Out of the original if, else statement
     numberofpeopleontheelevator = numberofpeopleontheelevator + int(input('How many people have entered the elevator: '))  # Adds the new amount of people that enter to the old amount. Use a negative number if people leave
-
+#test
